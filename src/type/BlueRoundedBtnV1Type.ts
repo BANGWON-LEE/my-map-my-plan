454ecb0b9169efa6d96aa2c66921c77981c32f14
BANGWON LEE <qkddnjs31@gmail.com>
2025-05-16 16:37:10 +0900

    edit : 주석 제거

--- src/type/BlueRoundedBtnV1Type.ts
+++ src/type/BlueRoundedBtnV1Type.ts
@@ -1,5 +1,5 @@
 export interface BlueRoundedBtnV1Type {
-  onClick: () => void // 함수 타입
-  text: string // 첫 번째 문자열
-  btnPosition: string // 두 번째 문자열
+  onClick: () => void
+  text: string
+  btnPosition: string
 }
