212f5b9dc572fdb8e07667975397c8280e160ffa
BANGWON LEE <qkddnjs31@gmail.com>
2025-05-19 01:32:17 +0900

    add : 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,16 @@
+'use client'
+
+import { renderPlaceMarker } from '@/actions/map-action/mapFunctions'
 import LogoTextField from '../common/input/LogoTextField'
 
 export default function Header() {
+  const getPlaceList = renderPlaceMarker
+
   return (
     <header className="absolute top-0 w-full h-[7rem] z-10 bg-transparent ">
       <div className="flex items-center m-auto w-3/4 h-full justify-evenly">
         <div className="w-3/5 h-full mt-4 ">
-          <LogoTextField />
+          <LogoTextField onClick={getPlaceList} />
         </div>
       </div>
     </header>
