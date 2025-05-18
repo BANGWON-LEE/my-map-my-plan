636ec0f8c816e99048c468c8f9963e0fe8800179
BANGWON LEE <qkddnjs31@gmail.com>
2025-05-19 01:31:32 +0900

    add: 타입 추가

---
+++ src/type/marker.ts
@@ -0,0 +1,12 @@
+// src/types/position.ts
+
+export interface simplePosition {
+  //   position: {
+  x: string
+  y: string
+  //   }
+}
+
+// export interface PositionType {
+//   position: GeolocationPosition | SimplePosition
+// }
