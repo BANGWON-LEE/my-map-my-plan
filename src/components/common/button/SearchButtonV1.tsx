a0377a02cd6344085ea56c36ae5e30ae84d2df25
BANGWON LEE <qkddnjs31@gmail.com>
2025-05-19 01:34:00 +0900

    edit: 검색 관련 컴포넌트 추가 및 함수 추가

--- src/components/common/button/SearchButtonV1.tsx
+++ src/components/common/button/SearchButtonV1.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
 import Search from '../../../assets/search.png'
+import { onClickEventProps } from '@/type/commonButton'
+// import { onClickProps } from '@/type/BlueRoundedBtnV1Type'
 
-export default function SearchButtonV1() {
+export default function SearchButtonV1(props: onClickEventProps) {
+  const { onClick } = props
+
   return (
-    <button className="w-full h-full relative">
+    <button onClick={onClick} className="w-full h-full relative">
       <Image fill src={Search} alt="MyMap 장소 검색 버튼" />
     </button>
   )
--- src/components/common/input/LogoTextField.tsx
+++ src/components/common/input/LogoTextField.tsx
@@ -1,17 +1,29 @@
 import Logo from '../logo/Logo'
 import SearchButtonV1 from '../button/SearchButtonV1'
+import { useState } from 'react'
+import { onClickProps } from '@/type/commonButton'
 
-export default function LogoTextField() {
+export default function LogoTextField(props: onClickProps) {
+  const { onClick } = props
+
+  const [text, setText] = useState<string>('')
+
+  function writeText(event: React.ChangeEvent<HTMLInputElement>) {
+    setText(event.currentTarget.value)
+  }
+
   return (
     <div className="flex w-full h-full  border-2 border-blue-400 rounded-2xl justify-between bg-[#fff]">
       <Logo />
       <input
         type="text"
+        value={text}
+        onChange={event => writeText(event)}
         placeholder="검색어를 입력하세요"
         className="outline-0 w-5/7 text-2xl font-bold"
       />
       <div className="w-[6rem] h-full">
-        <SearchButtonV1 />
+        <SearchButtonV1 onClick={() => onClick(text)} />
       </div>
     </div>
   )
