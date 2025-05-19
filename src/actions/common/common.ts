5c716acb50dbcc37fb40aba19e86c1e2c104c41e
BANGWON LEE <qkddnjs31@gmail.com>
2025-05-19 16:50:08 +0900

    add : 공용 함수 추가

--- src/actions/common/common.ts
+++ src/actions/common/common.ts
@@ -3,3 +3,5 @@ export function checkEmptyString(text: string) {
   const textStatus = text === '' || text === null || text === undefined
   if (textStatus) return alert('검색어를 입력해주세요')
 }
+
+export const formatMyLocation = (value: number): number => value / 1e7
