c431de7904b8dbd8cf643b472e680c3f9ac6c936
BANGWON LEE <qkddnjs31@gmail.com>
2025-05-19 16:49:54 +0900

    add: api 추가

---
+++ src/actions/api/api.ts
@@ -0,0 +1,19 @@
+'use server'
+import axios from 'axios'
+
+export async function getSearchLoc(text: string = '광화문') {
+  // axios.get(`https://openapi.naver.com/v1/search/local?query=${word}`)
+  const result = await axios.get(
+    `https://openapi.naver.com/v1/search/local?query=${text}&display=10`,
+    {
+      headers: {
+        'X-Naver-Client-Id': process.env.NAVER_CLIENT_ID!,
+        'X-Naver-Client-Secret': process.env.NAVER_CLIENT_SECRET!,
+      },
+    }
+  )
+
+  console.log('검색결과', result.data)
+
+  return result.data
+}
