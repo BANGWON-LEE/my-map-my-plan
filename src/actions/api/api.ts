09db0f385cfdef963bc78cbc51986a1e1c538630
BANGWON LEE <qkddnjs31@gmail.com>
2025-05-20 20:06:44 +0900

    add : 이미지 불러오는 함수 추가

--- src/actions/api/api.ts
+++ src/actions/api/api.ts
@@ -15,3 +15,18 @@ export async function getSearchLoc(text: string = '광화문') {
 
   return result.data
 }
+
+export async function getSearchLocImage(text: string = '광화문') {
+  // axios.get(`https://openapi.naver.com/v1/search/local?query=${word}`)
+  const result = await axios.get(
+    `https://openapi.naver.com/v1/search/image?query=${text}&display=5`,
+    {
+      headers: {
+        'X-Naver-Client-Id': process.env.NAVER_CLIENT_ID!,
+        'X-Naver-Client-Secret': process.env.NAVER_CLIENT_SECRET!,
+      },
+    }
+  )
+
+  return result.data
+}
