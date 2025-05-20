19d7fb0b06b9a75fd220ca96202a868d0ec4b631
BANGWON LEE <qkddnjs31@gmail.com>
2025-05-20 20:06:30 +0900

    add : 장소 타이틀 포맷 수정 함수 추가

--- src/actions/common/common.ts
+++ src/actions/common/common.ts
@@ -1,7 +1,11 @@
 export function checkEmptyString(text: string) {
-  console.log('check T', text)
   const textStatus = text === '' || text === null || text === undefined
   if (textStatus) return alert('검색어를 입력해주세요')
 }
 
 export const formatMyLocation = (value: number): number => value / 1e7
+
+export function formatAddressTitle(title: string, charsToRemove: string) {
+  const removePattern = new RegExp(`[${charsToRemove}]`, 'g')
+  return title.replace(removePattern, '')
+}
