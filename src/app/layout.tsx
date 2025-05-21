bc718e8c50fedcab2adead85d2dba602204a8ae5
BANGWON LEE <qkddnjs31@gmail.com>
2025-05-21 16:09:49 +0900

    edit : 로고 변경 및 아날리스트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import Main from '@/components/Main'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/next'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -33,7 +34,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Main>{children}</Main>
+        <Main>
+          {children}
+          <Analytics />
+        </Main>
       </body>
     </html>
   )
