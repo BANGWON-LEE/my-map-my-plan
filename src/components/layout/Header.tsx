2d7b9d10614a54ad87fdc48b695cd35775858bf1
BANGWON LEE <qkddnjs31@gmail.com>
2025-06-03 19:20:24 +0900

    edit : 출발과 도착 부분 작업

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,8 @@
-// 'use client'
+'use client'
 
 import { onClickPropsKeyBoadEventType } from '@/type/commonButton'
-import LogoTextField from '../common/input/LogoTextField'
+// import LogoTextField from '../common/input/LogoTextField'
+import HeaderInputField from '../common/input/HeaderInputFiled'
 
 export default function Header(props: onClickPropsKeyBoadEventType) {
   const { onClick, onKeyDown } = props
@@ -12,7 +13,7 @@ export default function Header(props: onClickPropsKeyBoadEventType) {
     <header className="absolute top-0 w-full h-[7rem] z-10 bg-transparent ">
       <div className="flex items-center m-auto w-3/4 h-full justify-evenly">
         <div className="w-3/5 h-full mt-4 ">
-          <LogoTextField onClick={getPlaceList} onKeyDown={onKeyDown} />
+          <HeaderInputField onClick={getPlaceList} onKeyDown={onKeyDown} />
         </div>
       </div>
     </header>
