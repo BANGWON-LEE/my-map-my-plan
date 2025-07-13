3d5fd3444511311a9a771f67e177c4e1d7be3ef0
BANGWON LEE <qkddnjs31@gmail.com>
2025-07-13 21:03:03 +0900

    edit : 불필요한 코드 제거

--- src/components/common/input/StartAndGoalTextField.tsx
+++ src/components/common/input/StartAndGoalTextField.tsx
@@ -2,114 +2,13 @@
 
 import { useRecoilValue } from 'recoil'
 import { routeGoalSelector, routeStartSelector } from '@/recoil/selector'
-import RouteChoiceBox from './RouteChoiceBox'
+import RouteChoiceBox from '../../modal/RouteChoiceBox'
 // import { start } from 'repl'
 // import { getPathDriving, getPathWalking } from '@/pages/api/searchLoc'
 
 export default function StartAndGoalTextField() {
   const startInfoState = useRecoilValue(routeStartSelector)
   const goalInfoState = useRecoilValue(routeGoalSelector)
-
-  // console.log('저장 후 탐색', startInfoState, goalInfoState)
-
-  // const [, setStartSummaryState] = useRecoilState(startLocSummaryAtom)
-
-  // const [, setRoutePathSignal] = useRecoilState(signalRouteStateAtom)
-  // const [, setCategoryState] = useRecoilState(signalCateGoryStateAtom)
-
-  // const [routeTypeState, setRouteTypeState] = useRecoilState(routeMethodAtom)
-  // const [, setOpenPlaceListModal] = useRecoilState<boolean>(
-  //   openPlaceListModalAtom
-  // )
-
-  // function drawPolyLine(
-  //   // position: routePositionType,
-  //   path: tmapRoutePathType,
-  //   polyLine: (map: naver.maps.Map, path: [[number, number]]) => void,
-  //   method: string
-  // ) {
-  //   const position = {
-  //     x: goalInfoState.goal.path.x,
-  //     y: goalInfoState.goal.path.y,
-  //   }
-
-  //   const map = onLoadRouteMap(position)
-  //   setRoutePathSignal(false)
-  //   polyLine(map, path.path)
-  //   startMarker(map, startInfoState.start.path)
-  //   goalMarker(map, goalInfoState.goal.path)
-  //   setCategoryState(placeListModalCategory.route)
-  //   setStartSummaryState({
-  //     distance: path.summary.totalDistance,
-  //     duration: path.summary.totalTime * 1000,
-  //     method: method,
-  //   })
-  //   setOpenPlaceListModal(true)
-  // }
-
-  // async function getPathDrivie(
-  //   startInfoState: startInfoType,
-  //   goalInfoState: goalInfoType
-  // ) {
-  //   setRouteTypeState('자동차')
-  //   setRoutePathSignal(true)
-  //   const requestData = {
-  //     startX: startInfoState.start.path.x,
-  //     startY: startInfoState.start.path.y,
-  //     endX: goalInfoState.goal.path.x,
-  //     endY: goalInfoState.goal.path.y,
-  //     reqCoordType: 'WGS84GEO',
-  //     resCoordType: 'WGS84GEO',
-  //     startName: startInfoState.start.name,
-  //     endName: goalInfoState.goal.name,
-  //   }
-
-  //   const res = await fetch('/api/driving', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify(requestData),
-  //   })
-  //   const path = await res.json()
-  //   const method = '자동차'
-  //   drawPolyLine(path, setCarPolyLine, method)
-  // }
-
-  // async function getPathWalk(
-  //   startInfoState: startInfoType,
-  //   goalInfoState: goalInfoType
-  // ) {
-  //   setRouteTypeState('걷기')
-  //   setRoutePathSignal(true)
-
-  //   const requestData = {
-  //     startX: startInfoState.start.path.x,
-  //     startY: startInfoState.start.path.y,
-  //     endX: goalInfoState.goal.path.x,
-  //     endY: goalInfoState.goal.path.y,
-  //     // passList: '경도,위도_경도,위도_경도,위도',
-  //     reqCoordType: 'WGS84GEO',
-  //     resCoordType: 'WGS84GEO',
-  //     startName: startInfoState.start.name,
-  //     endName: goalInfoState.goal.name,
-  //   }
-
-  //   // const path = await getPathWalking(requestData)
-  //   const res = await fetch('/api/walking', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify(requestData),
-  //   })
-  //   const path = await res.json()
-
-  //   const method = '도보'
-
-  //   drawPolyLine(path, setWalkPolyLine, method)
-  //   setOpenPlaceListModal(true)
-  // }
 
   const routeBtnStatus =
     startInfoState.start.name === '' || goalInfoState.goal.name === ''
--- src/components/modal/RouteHistoryComponent.tsx
+++ src/components/modal/RouteHistoryComponent.tsx
@@ -2,62 +2,9 @@
 import { placeListType } from '@/type/placeHistory'
 import React from 'react'
 import BlueRoundedBtnV1 from '../common/button/BlueRoundedBtnV1'
-// import { useRecoilValue } from 'recoil'
-// import { placeDistanceAtom } from '@/recoil/atoms'
-// import { placeDistanceType } from '@/type/route'
-// import { RouteDistanceComponent } from './RouteDistanceComponent'
 
 export default function RouteHistoryComponent(props: placeListType) {
   const { place, index, remove } = props
-
-  // const placeDistanceInfo = useRecoilValue(placeDistanceAtom)
-
-  // //   const routeDistanceArr: placeDistanceType[] = []
-  // const [routeDistanceArr, setRouteDistanceArr] = useState<placeDistanceType[]>(
-  //   []
-  // )
-
-  // function setDistanceArr(
-  //   // routeDistanceArr: placeDistanceType[],
-  //   placeDistanceInfo: placeDistanceType
-  // ) {
-  //   // console.log('placeDis', placeDistanceInfo)
-  //   setRouteDistanceArr(prev => [...prev, placeDistanceInfo])
-  // }
-
-  // useEffect(() => {
-  //   if (placeDistanceInfo) {
-  //     console.log('remove!!!', routeDistanceArr)
-  //     setDistanceArr(placeDistanceInfo)
-  //   }
-  // }, [placeDistanceInfo])
-
-  // const routeDistanceMatched =
-  //   routeDistanceArr.length > 0 &&
-  //   routeDistanceArr.find(el => el.start === place.name)
-
-  // function updateDistanceArr(
-  //   // routeDistanceArr: placeDistanceType[],
-  //   placeDistanceInfo: placeDistanceType[]
-  // ) {
-  //   console.log('라우트 99999')
-  //   // routeDistanceArr.length = 0
-  //   setRouteDistanceArr(placeDistanceInfo)
-  // }
-
-  // function removeDistance(index: number, placeName: string) {
-  //   const filterDistanceArr = routeDistanceArr.filter(
-  //     (distance, routeIndex) =>
-  //       distance.start !== placeName && distance.goal !== placeName
-  //   )
-
-  //   console.log('fffqqq', filterDistanceArr)
-  //   // routeDistanceArr.splice(removeIndex, 1)
-  //   updateDistanceArr(filterDistanceArr)
-  // }
-
-  // console.log('거리 배열 확인 중', routeDistanceArr)
-  // console.log('라우트 확인 중', routeDistanceMatched)
 
   return (
     <>
@@ -79,9 +26,6 @@ export default function RouteHistoryComponent(props: placeListType) {
           />
         </div>
       </div>
-      {/* {routeDistanceMatched && (
-        <RouteDistanceComponent placeDistanceInfo={routeDistanceMatched} />
-      )} */}
     </>
   )
 }
