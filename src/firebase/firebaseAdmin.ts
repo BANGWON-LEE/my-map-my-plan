13703bc11042c8678294529f041f8af68bc332ea
BANGWON LEE <qkddnjs31@gmail.com>
2025-08-25 20:30:15 +0900

    edit : admin 프라이빗 키 base64로 변경

--- src/firebase/firebaseAdmin.ts
+++ src/firebase/firebaseAdmin.ts
@@ -2,16 +2,20 @@
 import { getApps, initializeApp, cert } from 'firebase-admin/app'
 import { getFirestore } from 'firebase-admin/firestore'
 
+const privateKey = Buffer.from(
+  process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY!,
+  'base64'
+)
+  .toString('utf8')
+  .trim()
+
 const app = getApps().length
   ? getApps()[0]!
   : initializeApp({
       credential: cert({
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(
-          /\\n/g,
-          '\n'
-        )?.replace(/\r/g, ''),
+        privateKey: privateKey,
       }),
     })
 
